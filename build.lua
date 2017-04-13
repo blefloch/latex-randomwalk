efb2ee44175041bb16710a88e1c2ec77779c80f8
Bruno Le Floch <bruno@le-floch.fr>
2017-04-13 12:27:16 -0400

    Test that randomwalk loads without error nor warning

--- build.lua
+++ build.lua
@@ -1,6 +1,7 @@
 #!/usr/bin/env texlua
 -- Build script for randomwalk
 module = "randomwalk"
+checkengines = {"pdftex", "luatex", "xetex", "ptex", "uptex"}
 
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
