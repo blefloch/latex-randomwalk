73ade900c9402caefc9bdce09fb8e064aa381e40
Bruno Le Floch <bruno@le-floch.fr>
2018-12-28 10:39:52 +0100

    Code to upload to CTAN

--- build.lua
+++ build.lua
@@ -2,6 +2,20 @@
 -- Build script for randomwalk
 module = "randomwalk"
 checkengines = {"pdftex", "luatex", "xetex", "ptex", "uptex"}
+uploadconfig = {
+  author       = "Bruno Le Floch",
+  ctanPath     = "/macros/latex/contrib/randomwalk",
+  email        = "blflatex+randomwalk@gmail.com",
+  license      = "lppl1.3c",
+  pkg          = "randomwalk",
+  summary      = "Random walks using TikZ",
+  uploader     = "Bruno Le Floch",
+  version      = "0.6 2018-12-28",
+  bugtracker   = "https://github.com/blefloch/latex-randomwalk/issues",
+  description  = [[The randomwalk package provides a user command, \RandomWalk, to draw random walks with a given number of steps. Lengths and angles of the steps can be customized in various ways.
 
-kpse.set_program_name("kpsewhich")
-dofile(kpse.lookup("l3build.lua"))
+The package uses lcg for its ``random'' numbers and PGF/TikZ for its graphical output.]],
+  repository   = "https://github.com/blefloch/latex-randomwalk",
+  topic        = "random",
+  update       = true,
+}
